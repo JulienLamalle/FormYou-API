1b9f00d0c19537f0c7bff14e3c7d8dbc7ce53f47
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-11-30 12:53:50 +0100

    reviewing table formation_

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     # ASSOCIATIONS
+    has_many :formation_categories
     has_many :formations, through: :formation_categories
     # VALIDATIONS
     validates :name, presence: true
--- db/migrate/20201130111252_create_formation_categories.rb
+++ db/migrate/20201130111252_create_formation_categories.rb
@@ -3,7 +3,6 @@ class CreateFormationCategories < ActiveRecord::Migration[6.0]
     create_table :formation_categories do |t|
       t.references :formation, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
-
       t.timestamps
     end
   end
